2133eacc0165e7492c1fd78c9bd8c3506ba694d3
Antoine Cheminat <ac.dev@hotmail.com>
2022-08-22 18:08:47 -0400

    Create desktop menu

---
+++ src/lib/components/header/DesktopMenu.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+  import { menuItems } from "$lib/json";
+</script>
+
+<nav aria-label="Primary" class="hidden md:flex flex-1 justify-between ml-12">
+  <ul class="w-full flex items-center gap-4 text-neutral-200 font-bold">
+    {#each menuItems as { href, label } (label)}
+      <li><a {href} class="p-2 hover:text-neutral-300 transition-colors duration-300">{label}</a></li>
+    {/each}
+    <li class="ml-auto">
+      <a href="/" class="rounded-3xl p-2 hover:text-neutral-300 transition-colors duration-300">Login</a>
+    </li>
+    <li>
+      <a
+        href="/"
+        class="block rounded-3xl px-6 py-3 text-white bg-primary hover:opacity-70 transition-opacity duration-300"
+      >
+        Sign Up
+      </a>
+    </li>
+  </ul>
+</nav>
--- src/lib/components/header/Header.svelte
+++ src/lib/components/header/Header.svelte
@@ -1,11 +1,13 @@
 <script lang="ts">
   import Logo from "$lib/assets/logo.svg";
+  import DesktopMenu from "./DesktopMenu.svelte";
   import MobileMenu from "./MobileMenu.svelte";
 </script>
 
-<header class="container relative flex items-center justify-between py-2">
+<header class="container relative flex items-center">
   <a href="/" aria-label="Homepage">
     <Logo class="h-8 fill-neutral-400" />
   </a>
   <MobileMenu />
+  <DesktopMenu />
 </header>
