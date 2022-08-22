e98d386a1ea2eebca8da2990fec7570ad9baf0c5
Antoine Cheminat <ac.dev@hotmail.com>
2022-08-22 01:26:02 -0400

    Remove Vite Svelte SVG

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,5 @@
 import adapter from "@sveltejs/adapter-auto";
 import preprocess from "svelte-preprocess";
-import svelteSVG from "vite-plugin-svelte-svg";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -11,14 +10,6 @@ const config = {
   }),
 
   kit: {
-    vite: {
-      plugins: [
-        svelteSVG({
-          svgoConfig: {},
-          requireSuffix: false,
-        }),
-      ],
-    },
     adapter: adapter(),
   },
 };
