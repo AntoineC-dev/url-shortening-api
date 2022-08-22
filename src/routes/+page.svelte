f3cff02f577193b035e18e2117671f6022407f21
Antoine Cheminat <ac.dev@hotmail.com>
2022-08-22 00:25:43 -0400

    Initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,7 @@
+<svelte:head>
+  <title>Frontend Mentor | Shortly URL shortening API Challenge</title>
+</svelte:head>
+
+<main class="m-auto">
+  <h1 class="text-4xl">Hello World</h1>
+</main>
---
+++ tailwind.config.cjs
@@ -0,0 +1,35 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/app.html", "./src/**/*.{svelte,ts}"],
+  theme: {
+    screens: {
+      xs: "480px",
+      ...defaultTheme.screens,
+    },
+    colors: {
+      transparent: "transparent",
+      current: "currentColor",
+      primary: "hsl(180, 66%, 49%)",
+      secondary: "hsl(257, 27%, 26%)",
+      error: "hsl(0, 87%, 67%)",
+      neutral: {
+        100: "hsl(0, 0%, 75%)",
+        200: "hsl(257, 7%, 63%)",
+        300: "hsl(255, 11%, 22%)",
+        400: "hsl(260, 8%, 14%)",
+      },
+    },
+    fontWeight: {
+      normal: "500",
+      bold: "700",
+    },
+    extend: {
+      fontFamily: {
+        sans: ["Poppins", ...defaultTheme.fontFamily.sans],
+      },
+    },
+  },
+  plugins: [],
+};
