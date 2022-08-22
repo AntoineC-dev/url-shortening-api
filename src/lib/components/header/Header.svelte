3bd73015a02d51d8da1ee4ab84edffbde6b2a450
Antoine Cheminat <ac.dev@hotmail.com>
2022-08-22 19:03:41 -0400

    Create section styles

--- src/lib/components/header/Header.svelte
+++ src/lib/components/header/Header.svelte
@@ -4,10 +4,12 @@
   import MobileMenu from "./MobileMenu.svelte";
 </script>
 
-<header class="container relative flex items-center">
-  <a href="/" aria-label="Homepage" class="hover:opacity-70 transition-opacity duration-300">
-    <Logo class="h-8 fill-neutral-400" />
-  </a>
-  <MobileMenu />
-  <DesktopMenu />
+<header class="section">
+  <div class="container relative flex items-center">
+    <a href="/" aria-label="Homepage" class="hover:opacity-70 transition-opacity duration-300">
+      <Logo class="h-8 fill-neutral-400" />
+    </a>
+    <MobileMenu />
+    <DesktopMenu />
+  </div>
 </header>
