64e191b52046289ef4526b376605d014eec22ff9
Antoine Cheminat <ac.dev@hotmail.com>
2022-08-22 14:31:52 -0400

    Remove unused imports

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,12 +1,7 @@
-<script lang="ts">
-  import Logo from "$lib/assets/svg/logo.svg";
-</script>
-
 <svelte:head>
   <title>Frontend Mentor | Shortly URL shortening API Challenge</title>
 </svelte:head>
 
 <main class="m-auto">
   <h1 class="text-4xl">Hello World</h1>
-  <Logo class="w-96 fill-primary" />
 </main>
