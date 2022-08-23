f76d98cb852f58dd0ab278d1ea0389e57f1aef94
Antoine Cheminat <ac.dev@hotmail.com>
2022-08-23 17:16:07 -0400

    Add top padding

--- src/lib/components/header/Header.svelte
+++ src/lib/components/header/Header.svelte
@@ -4,7 +4,7 @@
   import MobileMenu from "./MobileMenu.svelte";
 </script>
 
-<header class="section">
+<header class="section pt-6 sm:pt-8 lg:pt-12">
   <div class="container relative flex items-center">
     <a href="/" aria-label="Homepage" class="hover:opacity-70 transition-opacity duration-300">
       <Logo class="h-8 fill-neutral-400" />
