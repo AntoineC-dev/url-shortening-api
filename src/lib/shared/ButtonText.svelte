a25f270802561b841437e9d066cb79113a7b270e
Antoine Cheminat <ac.dev@hotmail.com>
2022-08-25 05:54:35 -0400

    Create ButtonText component

---
+++ src/lib/shared/ButtonText.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+  export let size: "normal" | "dense" = "normal";
+  export let onClick: () => void;
+  export let ariaLabel: string | null = null;
+  export let disabled: boolean = false;
+  let props: string | undefined = undefined;
+  export { props as class };
+</script>
+
+<button
+  class:py-3={size === "dense"}
+  class:px-6={size === "dense"}
+  class:py-4={size === "normal"}
+  class:px-8={size === "normal"}
+  on:click={onClick}
+  aria-label={ariaLabel}
+  {disabled}
+  class="leading-none font-bold text-neutral-200 hover:enabled:text-neutral-300 transition-colors duration-300 whitespace-nowrap disabled:cursor-not-allowed {props}"
+>
+  <slot />
+</button>
--- src/lib/shared/index.ts
+++ src/lib/shared/index.ts
@@ -1,1 +1,2 @@
 export { default as Button } from "./Button.svelte";
+export { default as ButtonText } from "./ButtonText.svelte";
